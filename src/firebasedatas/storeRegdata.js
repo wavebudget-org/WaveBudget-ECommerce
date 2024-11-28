6e1688c670ccc40d33e5e4ac00775123b3e86843
temfa <akinfetemitope5@gmail.com>
2024-11-28 16:12:42 +0100

    Almost done

--- src/firebasedatas/storeRegdata.js
+++ src/firebasedatas/storeRegdata.js
@@ -1,31 +1,32 @@
-import { db, storage } from "./firebase";
-import { v4 } from "uuid";
+import { db } from "./firebase";
+// import { v4 } from "uuid";
 import { setDoc, doc } from "firebase/firestore";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+// import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export const saveData = async (id, payload) => {
   const userRef = doc(db, "userinformation", id);
 
+  console.log(payload);
   if (payload.cacImage) {
-    let result;
-    const imgRef = ref(storage, `images/${payload.cacImage + v4()}`);
+    // let result;
+    // const imgRef = ref(storage, `images/${payload.cacImage + v4()}`);
 
-    await uploadBytes(imgRef, payload.cacImage)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // await uploadBytes(imgRef, payload.cacImage)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
 
-    await getDownloadURL(imgRef)
-      .then((res) => {
-        console.log(res);
-        result = res;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // await getDownloadURL(imgRef)
+    //   .then((res) => {
+    //     console.log(res);
+    //     result = res;
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
 
     return setDoc(userRef, {
       email: payload.email,
@@ -37,12 +38,25 @@ export const saveData = async (id, payload) => {
       store: payload.store,
       address: payload.address,
       businessType: payload.businessType,
-      cac: result,
+      cac: "",
       businessDescription: payload.businessDescription,
       type: "Merchant",
     });
   } else {
-    return setDoc(userRef, payload);
+    return setDoc(userRef, {
+      email: payload.email,
+      userId: id,
+      phoneNumber: payload.phoneNumber,
+      acctname: payload.acctname,
+      acctnumber: payload.acctnumber,
+      bankname: payload.bankname,
+      store: payload.store,
+      address: payload.address,
+      businessType: payload.businessType,
+      cac: "",
+      businessDescription: payload.businessDescription,
+      type: "Merchant",
+    });
   }
 };
 
